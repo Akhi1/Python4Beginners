855011287238a10707fb7790899287b4cce043c7
Mita Samanta <mita.samanta@uflixdesign.com>
2017-10-26 18:19:37 +0530

    Added Toggle Menu function and some css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+$(document).ready( function(){
 
+    collapsible_menu();
+
+});
+
+function collapsible_menu() {
+
+    $('.collapsible-menu').each(function (i, e) {
+        $(e).on('click', 'li', function (e) {
+            $(this).children('ul').slideToggle();
+        });
+
+    });
+
+}
